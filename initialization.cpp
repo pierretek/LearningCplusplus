c2e7e78e1c29b713bbce3e6460e1b587622c3f00
pierrepressure <pierreawad10@gmail.com>
2025-10-01 10:36:09 -0400

    initial commit

---
+++ initialization.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+
+    //Default initialization
+    int a;
+    std::cout << "a equals "+a << std::endl;
+
+    //Straight copy initialization
+    int b = 5;
+    a = 1992;
+    std::cout << a << std::endl;
+    return 0;
+}
